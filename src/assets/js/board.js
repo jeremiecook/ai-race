48927bdc5b360b91c261a8cdc7a4c84642afdb0d
Jérémie COOK <hello@jeremiecook.com>
2017-11-12 14:13:49 +0100

    Reorganisation et Gulp

---
+++ src/assets/js/board.js
@@ -0,0 +1,60 @@
+/**
+ * Board game
+ */
+
+
+var BOARD_WIDTH  = 16000;
+var BOARD_HEIGHT = 9000;
+
+
+function Board () {
+
+	this.checkpoints = [];
+
+}
+
+Board.prototype = {
+
+	/**
+	 * Renvoie un checkpoint
+	 */
+	getCheckpoint: function (x) {
+		x = x % this.checkpoints.length; // Pour gérer les différents tours complets
+		return this.checkpoints[x];
+	},
+
+	/**
+	 * Création aléatoire de checkpoints
+	 */
+	randomCheckpoints: function () {
+
+		this.checkpoints = [];
+		this.checkpoints.push (new Checkpoint(0, 4000, 6000));
+		this.checkpoints.push (new Checkpoint(1, 9000, 2000));
+		this.checkpoints.push (new Checkpoint(2, 13000, 7000));
+
+
+		// Entre 3 et 5 checkpoints
+		// var n = Math.round ( Math.random () * 1 ) + 3;
+		// n = 3;
+
+		// for (var i = 0; i < n ; i++) {
+		// 	var x = Math.round( Math.random() * (BOARD_WIDTH - 1600)) + 800;
+		// 	var y = Math.round( Math.random() * (BOARD_HEIGHT - 1600)) + 800;
+		// 	this.checkpoints.push (new Checkpoint(i, x, y));
+		// }
+
+	},
+
+	/**
+	 * Affiche le board à l'écran
+	 */
+	display: function () {
+
+		// Affichage des checkpoints
+		for (i = 0; i < this.checkpoints.length; i++) {
+			this.checkpoints[i].display();
+		}
+	},
+
+};
---
+++ src/assets/js/utils.js
@@ -0,0 +1,57 @@
+/**
+ * Fonction de débug
+ */
+
+var lg = console.log.bind(console)
+
+/**
+ * Fonctions mathématiques
+ */
+
+/**
+ * Distance entre deux entités
+ */
+Math.distance = function (a, b) {
+	return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+/**
+ * Fonction de distribution
+ * Augement les probabilités des extrêmes (0 et 1)
+ */
+Math.ease = function (x, e = 2) {
+	return Math.pow(x,e) / ( Math.pow(x,e) + Math.pow(1 - x, e) );
+}
+
+/**
+ * Calcul d'un angle entre deux vecteurs
+ */
+Math.angle = function (a, b) {
+
+    ad = b.x - a.x;
+    op = b.y - a.y;
+    return Math.atan2( (op) , (ad) );
+
+  	// d = Math.distance(a, b);
+  	// dx = (b.x - a.x) / d;
+  	// dy = (b.y - a.y) / d;
+
+  	// a = Math.acos(dx);
+  	// a = dy < 0 ? 2 * Math.PI - a : a;
+  	// return a;
+
+}
+
+/**
+ * Conversion d'un angle en radians
+ */
+Math.radians = function ( degrees ) {
+	return degrees * Math.PI / 180;
+};
+
+/**
+ * Conversion d'un angle en degrés
+ */
+Math.degrees = function ( radians ) {
+	return radians * 180 / Math.PI;
+};
